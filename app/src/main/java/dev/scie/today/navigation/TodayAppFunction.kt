140a7f0c259c21fb5aa2bf1f4987f2e7c033b36a
Erick Howard <needlesslygrim@proton.me>
2024-06-16 20:39:51 +0800

    ui: change exam timetable icon to calendar now calendar function is gone
    Signed-off-by: Erick Howard <needlesslygrim@proton.me>

--- app/src/main/java/dev/scie/today/navigation/TodayAppFunction.kt
+++ app/src/main/java/dev/scie/today/navigation/TodayAppFunction.kt
@@ -15,7 +15,7 @@ enum class TodayAppFunction(
 ) {
 	/** Show CMS in a web view. */
 	WEB_CMS(R.drawable.ic_web_cms_unfilled, R.string.web_cms),
-	EXAM_TIMETABLE(R.drawable.ic_hourglass_empty_unfilled, R.string.exam_timetable),
+	EXAM_TIMETABLE(R.drawable.ic_calendar_month_unfilled, R.string.exam_timetable),
 	ATTENDANCE(R.drawable.ic_attendance_unfilled, R.string.attendance),
 	ANNOUNCEMENTS(R.drawable.ic_breaking_news_unfilled, R.string.announcements),
 	ECA(R.drawable.ic_eca_unfilled, R.string.subject_eca),
